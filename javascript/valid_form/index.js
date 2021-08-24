a6f0fed594476061ce6cec880877b025f042d454
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-08-24 13:56:04 +0530

    making validation form using javascript

---
+++ javascript/valid_form/index.js
@@ -0,0 +1,43 @@
+function clearErrors(){
+	erros = document.getElementsByClassName('form_error');
+	for(let item of erros){
+		item.innerHTML = '';
+	}
+}
+
+function form_error(id, error){
+	element = document.getElementById(id);
+	element.getElementsByClassName('form_error')[0].innerHTML = error;
+}
+
+function validateForm() {
+	var returnvalue = true;
+	clearErrors();
+	var name = document.forms['myform']['fname'].value;
+	if(name.length < 5){
+		form_error('name',' length of Name too sort');
+		document.getElementById('nm').style.border = '2px solid red';
+		returnvalue = false;
+	}
+
+	if(name.length == 0){
+		form_error('name',' length of Name can not to zero');
+		returnvalue = false;
+	}
+
+	var email = document.forms['myform']['femail'].value;
+	if(email.length < 10){
+		form_error('email',' Email is sort');
+		document.getElementById('em').style.border = '2px solid red';
+		returnvalue = false;
+	}
+
+	var pass = document.forms['myform']['fpassword'].value;
+	if(pass.length < 8){
+		form_error('password',' Password of too Much more');
+		document.getElementById('ps').style.border = '2px solid red';
+		returnvalue = false;
+	}
+
+	return returnvalue;
+}

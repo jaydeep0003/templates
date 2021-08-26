9f54d92734df0d3c32b6933be01a9e70e5d5036c
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-08-26 18:00:02 +0530

    Modified

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -1,8 +1,11 @@
 
-const items = ['jaydeep', 'vaghela']
-
-for(i in items){
-	console.log(i)
+const items = ['items_first', 'items_second']
+for(var i of items){
+var k = '';
+	for(var j of i){
+		k += j;
+	}
+	document.getElementById('search').placeholder = k;
+	console.log(k)
+	
 }
-
-// document.getElementById('search').placeholder = 'jaydeep'

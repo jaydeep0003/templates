ed7be08893ce50eeec1c8cd59e435bf7dff1059f
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-09-02 18:00:01 +0530

    Modified

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -17,49 +17,47 @@ const data = [
 	["Gujarat", 123, 234, 345, 456],
 	["Gujarat", 123, 234, 345, 456],
 	["Gujarat", 123, 234, 345, 456],
-	["Rajkot", 583, 548841, 1245, 52486],
 	["Gujarat", 123, 234, 345, 456],
 	["Gujarat", 123, 234, 345, 456],
 	["Gujarat", 123, 234, 345, 456],
 	["Gujarat", 123, 234, 345, 456],
 	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
+	["Gondal", 123, 234, 345, 456],
+	["Rajkot", 123, 234, 345, 450006],
 ]
 
-inner = document.getElementById("cell").innerHTML;
-for(item=0; item<data.length; item++)
-{
-	console.log(item, data[item]);
-	inner += `
+
+var newdata = '';
+var store =document.getElementById("cell").innerHTML; 
+data.forEach((value) => {
+		console.log(value[0])
+	store = `
 	<div class="cell">
-		<div class="state_name" id="table-first-value" value=''>${data[item][0]}</div>
+		<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
 	</div>
 
 	<div class="cell statistic">
-		<div class="delta is-confirmed" class="state_name" id="table-second-value" value=''>↑4,456</div>
-		<div>${data[item][1]}</div>
+		<div class="delta is-confirmed" class="state_name" id="table-second-value" value=''>${value[1]}</div>
+		<div>64,69,332</div>
 	</div>
 
 	<div class="cell statistic">
-		<div id="table-third-value" class="state_name" value=''>${data[item][2]}</div>
+		<div id="table-third-value" class="state_name" value=''>${value[2]}</div>
 	</div>
 
 	<div class="cell statistic">
-		<div class="delta is-recovered" class="state_name" id="table-fourth-value" value=''>↑5,156</div>
-		<div>${data[item][3]}</div>
+		<div class="delta is-recovered" class="state_name" id="table-fourth-value" value=''>${value[3]}</div>
+		<div>62,700,332</div>
 	</div>
 
 	<div class="cell statistic">
-		<div class="delta is-deceased" class="state_name" id="table-fifth-value" value=''>↑183</div>
-		<div>${data[item][4]}</div>
+		<div class="delta is-deceased" class="state_name" id="table-fifth-value" value=''>↑${value[4]}</div>
+		<div>1,37,332</div>
 	</div>
 	`
-}
+	newdata += store;
 
-document.getElementById("cell").innerHTML = inner;
+	});
+	
+document.getElementById("cell").innerHTML = newdata;
+

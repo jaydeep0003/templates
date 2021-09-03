81d5590a0b609dca4f0bcecbdae72610b056ed1b
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-09-03 16:00:02 +0530

    Modified

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -12,18 +12,18 @@
 // }
 
 const data = [
-	["rj", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gujarat", 123, 234, 345, 456],
-	["Gondal", 123, 234, 345, 456],
-	["Rajkot", 123, 234, 345, 450006],
+	["rj", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gujarat", 123, 234, 345, 456, 'jaydeep'],
+	["Gondal", 123, 234, 345, 456, 'jaydeep'],
+	["Rajkot", 123, 234, 345, 450006, 'jaydeep'],
 ]
 
 var newdata = '';
@@ -32,26 +32,30 @@ data.forEach((value,index) => {
 	if(index % 2 == 0) {
 		store = `
 		
-	<div class="cell">
+	<div class="cell fixed">
 		<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
 	</div>
 
 	<div class="cell statistic">
-		<div class="delta is-confirmed"  id="table-second-value" value=''>${value[1]}</div>
+		<div class="delta is-confirmed" >${value[1]}</div>
 		<div>64,69,332</div>
 	</div>
 
 	<div class="cell statistic">
-		<div id="table-third-value"  value=''>${value[2]}</div>
+		<div value=''>${value[2]}</div>
 	</div>
 
 	<div class="cell statistic">
-		<div class="delta is-recovered"  id="table-fourth-value" value=''>${value[3]}</div>
+		<div class="delta is-recovered">${value[3]}</div>
 		<div>62,700,332</div>
 	</div>
 
 	<div class="cell statistic">
-		<div class="delta is-deceased"  id="table-fifth-value" value=''>${value[4]}</div>
+		<div class="delta is-deceased">${value[4]}</div>
+		<div>1,37,332</div>
+	</div>
+	<div class="cell statistic ">
+		<div class="delta is-deceased">${value[5]}</div>
 		<div>1,37,332</div>
 	</div>
 	
@@ -62,26 +66,31 @@ data.forEach((value,index) => {
 	{
 		store = `
 
-			<div class="cell">
+			<div class="cell fixed">
 				<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
 			</div>
 
 			<div class="cell statistic new_class " >
-				<div class="delta is-confirmed" id="table-second-value" value=''>${value[1]}</div>
+				<div class="delta is-confirmed">${value[1]}</div>
 				<div>64,69,332</div>
 			</div>
 
 			<div class="cell statistic new_class ">
-				<div id="table-third-value" value=''>${value[2]}</div>
+				<div>${value[2]}</div>
 			</div>
 
 			<div class="cell statistic new_class ">
-				<div class="delta is-recovered" id="table-fourth-value" value=''>${value[3]}</div>
+				<div class="delta is-recovered">${value[3]}</div>
 				<div>62,700,332</div>
 			</div>
 
 			<div class="cell statistic new_class ">
-				<div class="delta is-deceased" id="table-fifth-value" value=''>${value[4]}</div>
+				<div class="delta is-deceased">${value[4]}</div>
+				<div>1,37,332</div>
+			</div>
+
+			<div class="cell statistic new_class ">
+				<div class="delta is-deceased">${value[5]}</div>
 				<div>1,37,332</div>
 			</div>
 		`	
@@ -89,7 +98,6 @@ data.forEach((value,index) => {
 	newdata += store;
 	});
 	
-
 	document.getElementById("cell").innerHTML = newdata;
 
 

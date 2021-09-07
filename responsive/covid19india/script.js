5d18da553ed683e1b4795abd29d4c1726acbb2d4
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-09-07 14:00:01 +0530

    Setting-Up as Per Latest Need

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -1,16 +1,3 @@
-
-// const items = ['first','second']
-// for(var i of items){
-// 	let k = '';
-// 	for(var j of i){
-// 		k += j;
-// 		setTimeout(()=>{
-// 			document.getElementById('search').value = k;
-// 			console.log(document.getElementById('search').value = k);
-// 		},3000);
-// 	}
-// }
-
 const data = [
 	["Gujarat", 123, 234, 345, 456, 123456789],
 	["Uttar Pradesh", 123, 234, 345, 456, 123456789],

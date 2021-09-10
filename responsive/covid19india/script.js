249ed0602181aeef813f2d4a464b8863dcdb538f
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-09-10 13:00:02 +0530

    Refactoring

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -2,67 +2,31 @@ async function getData(){
 	
 
 	try {
-
 		let response = await fetch('https://data.covid19india.org/v4/min/data.min.json')
 		let fetch_data = await response.json()
-
 		for(let k of Object.keys(fetch_data)){
-			let new_data = fetch_data[k]
-			console.log(new_data)
-		}
+			let value = fetch_data[k].total
+			var st = value.confirmed
+			var reco = value.recovered
+			// console.log(st)
+			// console.log(reco)
+			console.log(value)
 
-		return fetch_data
-	}
+		var store =document.getElementById("cell").innerHTML; 
 
-	catch(error) {
-		console.log(error)
-	}
-
-
-}
-
-var dt = getData()
-console.log(dt)
-
-// for(let key of Object.keys(dt)){
-// 	console.log(key + " " + dt[key])
-// }
-
-
-
-
-const data = [
-	["Gujarat", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["UP", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["AP", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["TN", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["WB", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Delhi", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Rajasthan", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Punjab", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Haryana", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Assam", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Goa", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-	["Mizoram", 123, 234, 345, 450006, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
-]
-
-var newdata = '';
-var store =document.getElementById("cell").innerHTML; 
-data.forEach((value,index) => {
-	if(index % 2 == 0) {
-		store = `
-	<div class="table_row " id='table-row' onmouseover='first_hover("${value[0]}")'>
+				store = `
+	<div class="table_row " id='table-row' onmouseover='first_hover("${k}")'>
 		<div class="cell fixed dark_mode_cell" id = 'dropdown-menu'>
-			<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
+			<div class="state_name" id="table-first-value" value=''>${k}</div>
 		</div>
 
 		<div class="cell statistic u_color u_hover " id="hover-id">
-			<div class="delta is-confirmed" >${value[1]}</div>
+			<div class="delta is-confirmed" >${st}</div>
 			
 		</div>
 
 		<div class="cell statistic u_color u_hover ">
-			<div value=''>${value[2]}</div>
+			<div value=''>${value.reco}</div>
 		</div>
 
 		<div class="cell statistic u_color u_hover ">
@@ -109,72 +73,176 @@ data.forEach((value,index) => {
 
 	</div>
 	`
+
+
+
+		}
+
+		return fetch_data
 	}
 
-	else
-	{
-		store = `
-		<div class='table_row' id='table-row' onmouseover='first_hover("${value[0]}")'>
-			<div class="cell fixed dark_mode_cell" id='row-first-id'>
-				<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
-			</div>
-
-			<div class="cell statistic new_class u_hover u_color ligth_color" id="hover-id">
-				<div class="delta is-confirmed">${value[1]}</div>
-				
-			</div>
-
-			<div class="cell statistic new_class u_hover u_color ligth_color ">
-				<div>${value[2]}</div>
-			</div>
-
-			<div class="cell statistic new_class u_hover u_color ">
-				<div class="delta is-recovered">${value[3]}</div>
-				
-			</div>
-
-			<div class="cell statistic new_class u_hover u_color ">
-				<div class="delta is-deceased">${value[4]}</div>
-				
-			</div>
-
-			<div class="cell statistic new_class u_hover u_color ">
-				<div class="delta is-active">${value[5]}</div>
-				
-			</div>
-			<div class="cell statistic u_hover u_color  new_class hide_cell">
-			<div class="delta is-deceased">${value[6]}</div>
-			
-		</div>
-		<div class="cell statistic u_hover u_color  new_class hide_cell">
-			<div class="delta is-deceased">${value[7]}</div>
-			
-		</div>
-		<div class="cell statistic u_hover u_color  new_class hide_cell">
-			<div class="delta is-deceased">${value[8]}</div>
-		</div>
-
-		<div class="cell statistic u_hover u_color  new_class hide_cell">
-			<div class="delta is-deceased">${value[9]}</div>
-		</div>
-
-		<div class="cell statistic u_hover u_color  new_class hide_cell">
-			<div class="delta is-deceased">${value[10]}</div>
-		</div>
-
-		<div class="cell statistic u_hover new_class hide_cell u_color ">
-			<div class="delta is-deceased">${value[11]}</div>
-		</div>
-
-		<div class="cell statistic u_hover new_class hide_cell u_color ">
-			<div class="delta is-deceased">${value[12]}</div>
-		</div>
-
-		</div>
-		`	
+	catch(error) {
+		console.log(error)
 	}
-	newdata += store;
-	});
+
+
+}
+
+var dt = getData()
+console.log(dt)
+
+
+
+
+	
+
+
+
+
+const data = [
+	["Gujarat", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["UP", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["AP", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["TN", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["WB", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Delhi", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Rajasthan", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Punjab", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Haryana", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Assam", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Goa", 123, 234, 345, 456, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+	["Mizoram", 123, 234, 345, 450006, 123456789, 'Gondal', 'india', 'US', '5.5Cr', '35687', '6.9Cr', '1.9Cr'],
+]
+
+var newdata = '';
+// // var store =document.getElementById("cell").innerHTML; 
+// // data.forEach((value,index) => {
+// // 	if(index % 2 == 0) {
+// // 		store = `
+// // 	<div class="table_row " id='table-row' onmouseover='first_hover("${value[0]}")'>
+// // 		<div class="cell fixed dark_mode_cell" id = 'dropdown-menu'>
+// // 			<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover " id="hover-id">
+// // 			<div class="delta is-confirmed" >${value[1]}</div>
+			
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover ">
+// // 			<div value=''>${value[2]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover ">
+// // 			<div class="delta is-recovered">${value[3]}</div>
+			
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover ">
+// // 			<div class="delta is-deceased">${value[4]}</div>
+			
+// // 		</div>
+// // 		<div class="cell statistic u_color u_hover ">
+// // 			<div class="delta is-active">${value[5]}</div>
+			
+// // 		</div>
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[6]}</div>
+			
+// // 		</div>
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[7]}</div>
+			
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[8]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[9]}</div>
+// // 		</div>	
+				
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[10]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[11]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_color u_hover  hide_cell">
+// // 			<div class="delta is-deceased">${value[12]}</div>
+// // 		</div>
+
+// // 	</div>
+// // 	`
+// // 	}
+
+// // 	else
+// // 	{
+// // 		store = `
+// // 		<div class='table_row' id='table-row' onmouseover='first_hover("${value[0]}")'>
+// // 			<div class="cell fixed dark_mode_cell" id='row-first-id'>
+// // 				<div class="state_name" id="table-first-value" value=''>${value[0]}</div>
+// // 			</div>
+
+// // 			<div class="cell statistic new_class u_hover u_color ligth_color" id="hover-id">
+// // 				<div class="delta is-confirmed">${value[1]}</div>
+				
+// // 			</div>
+
+// // 			<div class="cell statistic new_class u_hover u_color ligth_color ">
+// // 				<div>${value[2]}</div>
+// // 			</div>
+
+// // 			<div class="cell statistic new_class u_hover u_color ">
+// // 				<div class="delta is-recovered">${value[3]}</div>
+				
+// // 			</div>
+
+// // 			<div class="cell statistic new_class u_hover u_color ">
+// // 				<div class="delta is-deceased">${value[4]}</div>
+				
+// // 			</div>
+
+// // 			<div class="cell statistic new_class u_hover u_color ">
+// // 				<div class="delta is-active">${value[5]}</div>
+				
+// // 			</div>
+// // 			<div class="cell statistic u_hover u_color  new_class hide_cell">
+// // 			<div class="delta is-deceased">${value[6]}</div>
+			
+// // 		</div>
+// // 		<div class="cell statistic u_hover u_color  new_class hide_cell">
+// // 			<div class="delta is-deceased">${value[7]}</div>
+			
+// // 		</div>
+// // 		<div class="cell statistic u_hover u_color  new_class hide_cell">
+// // 			<div class="delta is-deceased">${value[8]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_hover u_color  new_class hide_cell">
+// // 			<div class="delta is-deceased">${value[9]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_hover u_color  new_class hide_cell">
+// // 			<div class="delta is-deceased">${value[10]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_hover new_class hide_cell u_color ">
+// // 			<div class="delta is-deceased">${value[11]}</div>
+// // 		</div>
+
+// // 		<div class="cell statistic u_hover new_class hide_cell u_color ">
+// // 			<div class="delta is-deceased">${value[12]}</div>
+// // 		</div>
+
+// // 		</div>
+// // 		`	
+// // 	}
+// 	newdata += store;
+// 	});
 	
 	document.getElementById("cell").innerHTML = newdata;
 

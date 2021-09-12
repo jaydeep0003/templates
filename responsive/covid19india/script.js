b2c670b8e1d8361aff817c64371d0c0f2c7dd7f4
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-09-12 15:00:01 +0530

    Temporary Commit

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -647,16 +647,15 @@ document.getElementById('themes').addEventListener('click', () => {
 		stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21
 		12.79z"></path></svg>`
 	}
+
 	else {
 		let svg_image = document.getElementById('themes');
 		svg_image.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"  fill="none" stroke="#ffc107" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`
 	}
 	
-
-		// if (document.body.) {
-
-		// }
 });
+
+
 
 
 
@@ -683,6 +682,45 @@ var active = 0
 
 
 document.getElementById('menu').addEventListener('click', (()=> {
-	document.getElementById('menu').classList.toggle('new_menu');	
-	console.log('clicked');
-}))
+	document.getElementById('hover-show').classList.toggle('new_menu');	
+
+	
+	if( document.getElementById('check-menu').innerHTML == 'Menu'){
+		 document.getElementById('check-menu').innerHTML = 'Close'
+	}
+	else {
+		 document.getElementById('check-menu').innerHTML = 'Menu'
+	}
+
+}));
+
+
+document.getElementById('themes-second').addEventListener('click', (() => {
+	document.body.classList.toggle('dark_mode');
+
+	if (document.body.classList.contains('dark_mode')) {
+		let svg_image = document.getElementById('themes-second');
+		svg_image.innerHTML = `<svg
+		xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+		fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+		stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21
+		12.79z"></path></svg>`
+	}
+
+	else {
+		let svg_image = document.getElementById('themes-second');
+		svg_image.innerHTML = `<svg
+		xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0
+		24 24"  fill="none" stroke="#ffc107" stroke-width="2"
+		stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12"
+		r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line
+		x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22"
+		x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78"
+		y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line
+		x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78"
+		x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78"
+		y2="4.22"></line></svg>` 
+	}
+	
+}));
+

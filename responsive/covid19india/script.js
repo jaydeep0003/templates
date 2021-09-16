945defe08dd8d4751543bac003e88b27726bf86d
jaydeep vaghela <jaydeepvaghela4148@gmail.com>
2021-09-16 16:00:01 +0530

    Cancelling Out Scrape Code

--- responsive/covid19india/script.js
+++ responsive/covid19india/script.js
@@ -2,6 +2,7 @@ field = ["delta21_14", "delta7", "districts", "meta", "total", "delta"]
 filed_item = ["other"]
 
 html_data = "";
+html_element_counter = 0
 fetch('https://data.covid19india.org/v4/min/data.min.json')
 .then(response => response.json())
 .then(data => {
@@ -18,109 +19,174 @@ fetch('https://data.covid19india.org/v4/min/data.min.json')
         var diff = filed_item.filter(i => !Object.keys(a).includes(i));
         var a = diff.reduce((j, d) => ({ ...j, [d]: 0}), a)
         
-        
-        html_element = `
-          <div class='table_row'>
+        console.log(state,state_data)
+        if (html_element_counter % 2 == 0) {
 
-            <div class="cell fixed dark_mode_cell" id='row-first-id'>
-              <div class="state_name" id="table-first-value" value=''>${state}</div>
+            html_element = `
+              <div class='table_row'>
+
+                <div class="cell fixed dark_mode_cell" id='row-first-id'>
+                  <div class="state_name" id="table-first-value" value=''>${state}</div>
+                </div>
+
+                <div class="cell statistic new_class u_hover u_color ligth_color" id="hover-id">
+                  <div class="delta is-confirmed">${state_data.total.confirmed}</div>
+                  
+                </div>
+
+                <div class="cell statistic new_class u_hover u_color ligth_color ">
+                  <div>${state_data.total.recovered}</div>
+                </div>
+
+                <div class="cell statistic new_class u_hover u_color ">
+                  <div class="delta is-recovered">${state_data.total.recovered}</div>
+                 
+                </div>
+
+                <div class="cell statistic new_class u_hover u_color ">
+                  <div class="delta is-deceased">${state_data.total.deceased}</div>
+                 
+                </div>
+
+                <div class="cell statistic new_class u_hover u_color ">
+                  <div class="delta is-active">${a.other}</div>
+                </div>
+
+                <div class="cell statistic u_hover u_color  new_class hide_cell">
+                  <div class="delta ">${state_data.total.tested}</div>
+                </div>
+
+                <div class="cell statistic u_hover u_color  new_class hide_cell">
+                  <div class="delta is_vaccine">${state_data.total.vaccinated1}</div>
+                  
+                </div>
+
+                <div class="cell statistic u_hover u_color  new_class hide_cell">
+                  <div class="delta is_vaccine">${state_data.total.vaccinated2}</div>
+                
+                </div>
+
+                <div class="cell statistic u_hover u_color  new_class hide_cell">
+                  <div class="delta is_vaccine">${state_data.total.vaccinated1 + state_data.total.vaccinated2}</div>
+                  
+                </div>
+
+                <div class="cell statistic u_hover u_color  new_class hide_cell">
+               
+                </div>
+
+                <div class="cell statistic u_hover new_class hide_cell u_color ">
+                  <div class="delta"></div>
+                </div>
+
+                <div class="cell statistic u_hover new_class hide_cell u_color ">
+                  <div class="delta">${state_data.meta.population}</div>
+                </div>
+
+              </div>
+            `
+          }
+          else
+          {
+            store = `
+            <div class='table_row'>
+              <div class="cell fixed dark_mode_cell" id='row-first-id'>
+                <div class="state_name" id="table-first-value" value=''>${state}</div>
             </div>
-
             <div class="cell statistic new_class u_hover u_color ligth_color" id="hover-id">
-              <div class="delta is-confirmed">${state_data.total.confirmed}</div>
               
+              <div class="delta is-confirmed">${new Intl.NumberFormat().format(state_data.total.confirmed)}</div>
             </div>
-
             <div class="cell statistic new_class u_hover u_color ligth_color ">
               <div>${state_data.total.recovered}</div>
             </div>
-
             <div class="cell statistic new_class u_hover u_color ">
               <div class="delta is-recovered">${state_data.total.recovered}</div>
-             
             </div>
 
             <div class="cell statistic new_class u_hover u_color ">
               <div class="delta is-deceased">${state_data.total.deceased}</div>
-             
+            
             </div>
-
             <div class="cell statistic new_class u_hover u_color ">
               <div class="delta is-active">${a.other}</div>
             </div>
-
             <div class="cell statistic u_hover u_color  new_class hide_cell">
-              <div class="delta ">${state_data.total.tasted}</div>
+              <div class="delta ">${state_data.total.tested}</div>
             </div>
-
             <div class="cell statistic u_hover u_color  new_class hide_cell">
-              <div class="delta is_vaccine"></div>
+              <div class="delta is_vaccine">${state_data.total.vaccinated1}</div>
               
             </div>
-
             <div class="cell statistic u_hover u_color  new_class hide_cell">
-              <div class="delta is_vaccine"></div>
-            
-            </div>
-
-            <div class="cell statistic u_hover u_color  new_class hide_cell">
-              <div class="delta is_vaccine"></div>
+              <div class="delta is_vaccine">${state_data.total.vaccinated2}</div>
               
             </div>
-
             <div class="cell statistic u_hover u_color  new_class hide_cell">
-           
+              <div class="delta is_vaccine">${state_data.total.vaccinated1 + state_data.total.vaccinated2}</div>
+               
             </div>
-
+            <div class="cell statistic u_hover u_color  new_class hide_cell">
+              <div class="delta"></div>
+            </div>
             <div class="cell statistic u_hover new_class hide_cell u_color ">
               <div class="delta"></div>
             </div>
-
             <div class="cell statistic u_hover new_class hide_cell u_color ">
-              <div class="delta"></div>
+              <div class="delta">${state_data.meta.population}</div>
             </div>
-
           </div>
-        `
+        ` 
+          }
 
         html_data += html_element;
+        html_element_counter += 1;
 
     }
 
     document.getElementById('main-table').innerHTML = html_data;
                     
-                    // Table Event
+                    // Table Events
     document.getElementById('right-arrow').addEventListener('click', right_arrow);
 
-    function right_arrow()
-    {
-      var right_arrow = document.getElementById('right-arrow');
-      var table = document.getElementById('table-container');
-      var hide_cell = document.getElementsByClassName('hide_cell')
-      if (right_arrow.classList.contains('right_arrow')) {
+   function right_arrow() {
+    var right_arrow = document.getElementById('right-arrow');
+    var table = document.getElementById('table-container');
+    var hide_cell = document.getElementsByClassName('hide_cell');
+    var main_hero_section = document.getElementById('hero');
+    if (right_arrow.classList.contains('right_arrow')) {
 
-            right_arrow.classList.remove('right_arrow');
-            right_arrow.classList.add('new_right_arrow');
+        right_arrow.classList.remove('right_arrow');
+        right_arrow.classList.add('new_right_arrow');
 
-            table.classList.remove('table_row');
-            table.classList.add('click_table');
+        for (var i of hide_cell) {
+            i.classList.add('show');
+        }
 
-            for(var i of hide_cell){
-                  i.classList.add('show')
-            }
+        table.classList.remove('table_row');
+        table.classList.add('click_table');
 
-      }
-      else
-      {
+        main_hero_section.classList.remove('hero')
+        main_hero_section.classList.add('new_hero');
+
+
+    } else {
         right_arrow.classList.remove('new_right_arrow');
         right_arrow.classList.add('right_arrow');
 
-        table.classList.remove('click_table')
-        table.classList.add('table_row')
-      }
+        for (var i of hide_cell) {
+            i.classList.remove('show');
+        }
 
+        table.classList.remove('click_table');
+        table.classList.add('table_row');
+
+        main_hero_section.classList.remove('new_hero');
+        main_hero_section.classList.add('hero')
 
     }
+
+  }
 
 });
 
